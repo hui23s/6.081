8387789e59f3122d2f05c7ea3184579cf0e9b97c
hui23s <1210708168@qq.com>
2023-06-09 10:32:03 +0800

    lab1: pingpong finish

---
+++ user/pingpong.c
@@ -0,0 +1,53 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int 
+main(int argc, char *argv[]){
+  int p1[2], p2[2];
+  pipe(p1);
+  pipe(p2);
+  char buf[1];
+
+  int pid;
+  if(fork() == 0){
+    pid = getpid();
+    
+    if(read(p1[0], buf, 1) != 1){
+      fprintf(2, "child read error\n");
+      exit(1);
+    }
+
+    close(p1[0]);
+    printf("%d: received ping\n", pid);
+
+    if(write(p2[1], buf, 1) != 1){
+      fprintf(2, "child write error\n");
+      exit(1);
+    }
+
+    close(p2[1]);
+    exit(0);
+
+  } else{
+    pid = getpid();
+    buf[0] = 'a';
+
+    if(write(p1[1], buf, 1) != 1){
+      fprintf(2, "parent write error\n");
+      exit(1);
+    }
+    
+    close(p1[1]);
+    wait(0);
+
+    if(read(p2[0], buf, 1) != 1){
+      fprintf(2, "parent read error\n");
+      exit(1);
+    }
+    close(p2[0]);
+    printf("%d: received pong\n", pid);
+  }
+
+  exit(0);
+}
